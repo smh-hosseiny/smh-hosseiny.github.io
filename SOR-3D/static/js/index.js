eb0573b349f530032bf480f59735b5771639c279
Seyed Mohammad Hossein Hosseini <hosseiny290@gmail.com>
2025-02-22 01:51:27 -0500

    add gitattributes

--- SOR-3D/static/js/index.js
+++ SOR-3D/static/js/index.js
@@ -1,7 +1,6 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-// var INTERP_BASE = "./static/interpolation/stacked";
-var INTERP_BASE = "./static/images";
+var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
